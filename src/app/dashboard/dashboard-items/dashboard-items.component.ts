e558fb14f9ef5e3902f36db39aa59b6f215498ef
Abdul Rahuman <abrahuman8870@gmail.com>
2024-06-17 22:39:05 -0400

    Added components and managed the code

---
+++ src/app/dashboard/dashboard-items/dashboard-items.component.ts
@@ -0,0 +1,17 @@
+import { Component ,Input,input} from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-items.component.html',
+  styleUrl: './dashboard-items.component.css'
+})
+export class DashboardItemsComponent {
+
+  // @Input({required:true}) image!:{src:string;alt:string;};
+  // @Input({required:true}) title!:string;
+
+  image =input.required<{src:string;alt:string}>();
+  title=input.required<string>();
+}
